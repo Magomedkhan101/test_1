f9292e19dd43359f38a7539c0a11c78aa406fa62
Khan <magomedkhankad@icloud.com>
2022-10-28 17:12:52 +0300

    main

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 
-
+def fibonacci(num):
+    fib_1 = 0
+    fib_2 = 1
+    print(f'{fib_1}\n{fib_2}')
+    while num > 2:
+        fib_1, fib_2 = fib_2, fib_1 + fib_2
+        num -= 1
+    return fib_2
